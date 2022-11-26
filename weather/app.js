09acb0f6ad22aca948e11ac85928581617601ddc
Sergey Lunev <lunev.s@gmail.com>
2022-11-26 23:24:34 +0300

    post added

---
+++ weather/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", (req, res) => {
+
+    const city = req.body.cityName;
+    const appKey = "3cceae3ba90456ed399fdd74602eb32f";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + appKey + "&units=metric";
+
+    https.get(url, (response) => {
+
+        console.log(response.statusCode);
+
+        response.on("data", (data) => {
+            let q = JSON.parse(data);
+            let icon = q.weather[0].icon;
+            let icon_url = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            let temp = q.main.temp;
+            let descr = q.weather[0].description;
+
+            res.write("<p>The weather in " + city + " is " + descr + "</p>");
+            res.write("<h1>The tempreature is " + temp + "</h1>");
+            res.write("<img src='" + icon_url + "' />");
+            res.send();
+
+        });
+    })
+})
+
+
+
+app.listen(3000, () => {
+    console.log("server is running at 3000");
+})
