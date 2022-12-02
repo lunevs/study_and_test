4ccaea36fdb6000a64ea22bfd7f6c29914182c7e
Sergey Lunev <lunev.s@gmail.com>
2022-12-02 15:37:16 +0300

    style css added

---
+++ tests/main.js
@@ -0,0 +1,24 @@
+function ttt(myVar) {
+    console.log(myVar);
+}
+const rrr = (ert) => {
+    console.log(ert);
+}
+
+const ppp = (p) => {
+    return p * p;
+}
+
+ttt(" se sehsf sdhs djsjdfjs");
+rrr("hg jdj gdjfgjd gdh dgd");
+
+ttt(15);
+rrr(`fjgdf gdjfhg djf gd ${ppp(34)} fgh `)
+
+console.log(typeof 1245223344234273647826347267836234627638472);
+console.log(typeof 1245223344234273647826347267836234627638472n);
+
+console.log(Symbol("4") == Symbol("5"));
+
+console.log(typeof Symbol("4"));
+
