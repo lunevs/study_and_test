c0ca3f120db137442d93162d2e7b6b92a2341198
Sergey Lunev <lunev.s@gmail.com>
2023-01-04 14:29:05 +0300

    update db engine

--- todo list/app.js
+++ todo list/app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-var items = [];
-var workItems = [];
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB');
+const Item = mongoose.model("Item", new Schema({ name: String }));
+const do1 = new Item({name: "wash my car"});
+const do2 = new Item({name: "buy tickets to the moon"});
+Item.insertMany([do1, do2], err => console.log(err));
 
 app.get("/", (req, res) => {
     let today = new Date();
