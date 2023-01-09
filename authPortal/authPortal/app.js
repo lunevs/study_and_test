a9555101562a54439011523d114a9386eb6407b2
Sergey Lunev <lunev.s@gmail.com>
2023-01-09 14:56:29 +0300

    add register and login function. Password as plant text

--- authPortal/authPortal/app.js
+++ authPortal/authPortal/app.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const { MONGODB_URI } = require("./config");
 
 const app = express();
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.set('strictQuery', false);
+mongoose.connect(MONGODB_URI);
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
    res.render("home");
@@ -17,7 +28,23 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-    
+
+    User.findOne({email: req.body.username}, (error, foundUser) => {
+        if (error) {
+            console.log(error)
+        } else {
+            if (foundUser) {
+                if (foundUser.password && foundUser.password === req.body.password) {
+                    res.render("secrets")
+                } else {
+                    res.send("Incorrect user password");
+                }
+            } else {
+                res.send("No such user");
+            }
+        }
+    });
+
 })
 
 app.get("/register", (req, res) => {
@@ -26,6 +53,18 @@ app.get("/register", (req, res) => {
 
 app.post("/register", (req, res) => {
 
+    const currentUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    console.log(currentUser);
+    currentUser.save((error) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.render("secrets");
+        }
+    });
 })
 
 app.listen(3000, () => {
